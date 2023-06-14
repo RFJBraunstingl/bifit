7112bcdf1a3e82c1695e9840384e5f2377bb01b6
Roman BRAUNSTINGL <r.braunstingl@celix.at>
2023-06-14 20:11:43 +0200

    add heap ticker

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -10,12 +10,42 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
 #include "esp_spi_flash.h"
+#include <esp_log.h>
+#include <esp_system.h>
 
 // #define BIFIT_CONFIG_ENABLE_DEBUG_LOGGING
 
 #include "bifit.h"
+
+void print_heap_usage() {
+        printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+}
+
+void periodically_print_heap_usage() {
+        const esp_timer_create_args_t timer_args = {
+                .callback = &print_heap_usage,
+                .name = "heap-ticker"
+        };
+
+        esp_timer_handle_t heap_tick_timer;
+        esp_err_t result = esp_timer_create(&timer_args, &heap_tick_timer);
+        if (ESP_OK != result) {
+                ESP_LOGE("main", "failed to register timer! %s", esp_err_to_name(result));
+                return;
+        }
+
+        ESP_LOGI("main", "heap ticker created!");
+
+        const uint64_t INTERVALL_IN_US = 10 * 1000;
+        result = esp_timer_start_periodic(heap_tick_timer, INTERVALL_IN_US);
+        if (ESP_OK != result) {
+                ESP_LOGE("main", "error starting heap ticker! %s", esp_err_to_name(result));
+                return;
+        }
+
+        ESP_LOGI("main", "heap ticker started!");
+}
 
 void app_main(void)
 {
@@ -33,7 +63,8 @@ void app_main(void)
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+    print_heap_usage();
+    periodically_print_heap_usage();
 
     printf("\n\n\n\n");
     printf("running bifit...\n");
