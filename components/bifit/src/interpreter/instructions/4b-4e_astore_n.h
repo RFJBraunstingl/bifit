f54b8e947604b88d95f2fc53de2fd8247b5ca43f
Roman BRAUNSTINGL <r.braunstingl@celix.at>
2023-06-14 22:38:49 +0200

    close last memory leak

--- components/bifit/src/interpreter/instructions/4b-4e_astore_n.h
+++ components/bifit/src/interpreter/instructions/4b-4e_astore_n.h
@@ -3,8 +3,10 @@
 unsigned int bifit_execute_instruction_astore_n(unsigned int pc, bifit_stack_frame_t *stack_frame, int n) {
     BIFIT_LOG_DEBUG("Store reference into local variable\n");
 
-    bifit_operand_t *top_operand = bifit_stack_pop(&(stack_frame->operand_stack));
-    stack_frame->local_variable_array[n].object_reference = top_operand->object_reference;
+    bifit_object_reference_t *object_reference = bifit_operand_stack_pop_reference(
+            &(stack_frame->operand_stack)
+    );
+    stack_frame->local_variable_array[n].object_reference = object_reference;
 
     return pc;
 }
